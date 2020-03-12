0cf87a82d508e5d24aedc4fd061a45549d55e218
Fabio Milheiro <fabiomilheiro@pikabytes.com>
2020-03-12 10:17:49 +0000

    Fixed private field nomenclature.

--- Moneybox.App.Tests/Features/TransferMoneyTests.cs
+++ Moneybox.App.Tests/Features/TransferMoneyTests.cs
@@ -10,15 +10,15 @@ namespace Moneybox.App.Tests.Features
 {
     public class TransferMoneyTests
     {
-        private TransferMoney _sut;
-        private Mock<IAccountRepository> _accountRepositoryMock;
-        private Mock<INotificationService> _notificationServiceMock;
-        private Account _account1, _account2;
-        private Account _updatedAccount1, _updatedAccount2;
+        private TransferMoney sut;
+        private Mock<IAccountRepository> accountRepositoryMock;
+        private Mock<INotificationService> notificationServiceMock;
+        private Account account1, account2;
+        private Account updatedAccount1, updatedAccount2;
 
         public TransferMoneyTests()
         {
-            _account1 = new Account
+            account1 = new Account
             {
                 Id = Guid.NewGuid(),
                 Balance = 1000,
@@ -31,7 +31,7 @@ namespace Moneybox.App.Tests.Features
                     Name = "John Smith"
                 }
             };
-            _account2 = new Account
+            account2 = new Account
             {
                 Id = Guid.NewGuid(),
                 Balance = 5000,
@@ -44,28 +44,28 @@ namespace Moneybox.App.Tests.Features
                     Name = "Mary Smith"
                 }
             };
-            _accountRepositoryMock = new Mock<IAccountRepository>();
-            _accountRepositoryMock
-                .Setup(r => r.GetAccountById(_account1.Id))
-                .Returns(_account1);
-            _accountRepositoryMock
-                .Setup(r => r.GetAccountById(_account2.Id))
-                .Returns(_account2);
+            accountRepositoryMock = new Mock<IAccountRepository>();
+            accountRepositoryMock
+                .Setup(r => r.GetAccountById(account1.Id))
+                .Returns(account1);
+            accountRepositoryMock
+                .Setup(r => r.GetAccountById(account2.Id))
+                .Returns(account2);
 
-            _accountRepositoryMock.Setup(r => r.Update(_account1)).Callback<Account>(acc => _updatedAccount1 = acc);
-            _accountRepositoryMock.Setup(r => r.Update(_account2)).Callback<Account>(acc => _updatedAccount2 = acc);
+            accountRepositoryMock.Setup(r => r.Update(account1)).Callback<Account>(acc => updatedAccount1 = acc);
+            accountRepositoryMock.Setup(r => r.Update(account2)).Callback<Account>(acc => updatedAccount2 = acc);
 
-            _notificationServiceMock = new Mock<INotificationService>();
+            notificationServiceMock = new Mock<INotificationService>();
 
-            _sut = new TransferMoney(
-                _accountRepositoryMock.Object,
-                _notificationServiceMock.Object);
+            sut = new TransferMoney(
+                accountRepositoryMock.Object,
+                notificationServiceMock.Object);
         }
 
         [Fact]
         public void Execute_NewBalanceIsNegative_Throws()
         {
-            Action action = () => _sut.Execute(_account1.Id, _account2.Id, _account1.Balance + 1);
+            Action action = () => sut.Execute(account1.Id, account2.Id, account1.Balance + 1);
 
             action.Should().Throw<InvalidOperationException>().WithMessage("Insufficient funds*");
         }
@@ -73,19 +73,19 @@ namespace Moneybox.App.Tests.Features
         [Fact]
         public void Execute_NewBalanceLessThan500_SendsNotification()
         {
-            _account1.Balance = 1000;
+            account1.Balance = 1000;
 
-            _sut.Execute(_account1.Id, _account2.Id, 501);
+            sut.Execute(account1.Id, account2.Id, 501);
 
-            _notificationServiceMock.Verify(s => s.NotifyFundsLow(_account1.User.Email), Times.Once);
+            notificationServiceMock.Verify(s => s.NotifyFundsLow(account1.User.Email), Times.Once);
         }
 
         [Fact]
         public void Execute_PayInLimitReached_Throws()
         {
-            _account2.PaidIn = Account.PayInLimit - 100;
+            account2.PaidIn = Account.PayInLimit - 100;
 
-            Action action = ()=> _sut.Execute(_account1.Id, _account2.Id, 101);
+            Action action = ()=> sut.Execute(account1.Id, account2.Id, 101);
 
             action.Should().Throw<InvalidOperationException>().WithMessage("Account pay in limit reached*");
         }
@@ -93,41 +93,41 @@ namespace Moneybox.App.Tests.Features
         [Fact]
         public void Execute_PayInToLimitDifferenceLessThan500_SendsNotification()
         {
-            _account2.PaidIn = Account.PayInLimit - 1000;
+            account2.PaidIn = Account.PayInLimit - 1000;
 
-            _sut.Execute(_account1.Id, _account2.Id, 501);
+            sut.Execute(account1.Id, account2.Id, 501);
 
-            _notificationServiceMock.Verify(s => s.NotifyApproachingPayInLimit(_account2.User.Email), Times.Once);
+            notificationServiceMock.Verify(s => s.NotifyApproachingPayInLimit(account2.User.Email), Times.Once);
         }
 
         [Fact]
         public void Execute_ValidationsPassed_UpdatesBothAccounts()
         {
-            _sut.Execute(_account1.Id, _account2.Id, 10);
+            sut.Execute(account1.Id, account2.Id, 10);
 
-            _updatedAccount1.Should().BeEquivalentTo(new Account
+            updatedAccount1.Should().BeEquivalentTo(new Account
             {
-                Id = _account1.Id,
+                Id = account1.Id,
                 Balance = 990,
                 PaidIn = 0,
                 Withdrawn = -10,
                 User = new User
                 {
-                    Id = _account1.User.Id,
+                    Id = account1.User.Id,
                     Email = "john.smith@test.com",
                     Name = "John Smith"
                 }
             });
 
-            _updatedAccount2.Should().BeEquivalentTo(new Account
+            updatedAccount2.Should().BeEquivalentTo(new Account
             {
-                Id = _account2.Id,
+                Id = account2.Id,
                 Balance = 5010,
                 PaidIn = 10,
                 Withdrawn = 0,
                 User = new User
                 {
-                    Id = _account2.User.Id,
+                    Id = account2.User.Id,
                     Email = "mary.smith@test.com",
                     Name = "Mary Smith"
                 }
