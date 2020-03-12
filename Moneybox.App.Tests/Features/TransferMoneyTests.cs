ba7236cf22d9a367bd43ba33b633637b68f57e52
Fabio Milheiro <fabiomilheiro@pikabytes.com>
2020-03-12 10:36:30 +0000

    Add "this." to instance fields to keep it consistent. Added failing tests to WithdrawMoneyTests.

--- Moneybox.App.Tests/Features/TransferMoneyTests.cs
+++ Moneybox.App.Tests/Features/TransferMoneyTests.cs
@@ -18,7 +18,7 @@ namespace Moneybox.App.Tests.Features
 
         public TransferMoneyTests()
         {
-            account1 = new Account
+            this.account1 = new Account
             {
                 Id = Guid.NewGuid(),
                 Balance = 1000,
@@ -31,7 +31,7 @@ namespace Moneybox.App.Tests.Features
                     Name = "John Smith"
                 }
             };
-            account2 = new Account
+            this.account2 = new Account
             {
                 Id = Guid.NewGuid(),
                 Balance = 5000,
@@ -44,28 +44,26 @@ namespace Moneybox.App.Tests.Features
                     Name = "Mary Smith"
                 }
             };
-            accountRepositoryMock = new Mock<IAccountRepository>();
-            accountRepositoryMock
-                .Setup(r => r.GetAccountById(account1.Id))
-                .Returns(account1);
-            accountRepositoryMock
-                .Setup(r => r.GetAccountById(account2.Id))
-                .Returns(account2);
+            this.accountRepositoryMock = new Mock<IAccountRepository>();
+            this.accountRepositoryMock
+                .Setup(r => r.GetAccountById(this.account1.Id))
+                .Returns(this.account1);
+            this.accountRepositoryMock
+                .Setup(r => r.GetAccountById(this.account2.Id))
+                .Returns(this.account2);
 
-            accountRepositoryMock.Setup(r => r.Update(account1)).Callback<Account>(acc => updatedAccount1 = acc);
-            accountRepositoryMock.Setup(r => r.Update(account2)).Callback<Account>(acc => updatedAccount2 = acc);
+            this.accountRepositoryMock.Setup(r => r.Update(this.account1)).Callback<Account>(acc => this.updatedAccount1 = acc);
+            this.accountRepositoryMock.Setup(r => r.Update(this.account2)).Callback<Account>(acc => this.updatedAccount2 = acc);
 
-            notificationServiceMock = new Mock<INotificationService>();
+            this.notificationServiceMock = new Mock<INotificationService>();
 
-            sut = new TransferMoney(
-                accountRepositoryMock.Object,
-                notificationServiceMock.Object);
+            this.sut = new TransferMoney(this.accountRepositoryMock.Object, this.notificationServiceMock.Object);
         }
 
         [Fact]
         public void Execute_NewBalanceIsNegative_Throws()
         {
-            Action action = () => sut.Execute(account1.Id, account2.Id, account1.Balance + 1);
+            Action action = () => this.sut.Execute(this.account1.Id, this.account2.Id, this.account1.Balance + 1);
 
             action.Should().Throw<InvalidOperationException>().WithMessage("Insufficient funds*");
         }
@@ -73,19 +71,19 @@ namespace Moneybox.App.Tests.Features
         [Fact]
         public void Execute_NewBalanceLessThan500_SendsNotification()
         {
-            account1.Balance = 1000;
+            this.account1.Balance = 1000;
 
-            sut.Execute(account1.Id, account2.Id, 501);
+            this.sut.Execute(this.account1.Id, this.account2.Id, 501);
 
-            notificationServiceMock.Verify(s => s.NotifyFundsLow(account1.User.Email), Times.Once);
+            this.notificationServiceMock.Verify(s => s.NotifyFundsLow(this.account1.User.Email), Times.Once);
         }
 
         [Fact]
         public void Execute_PayInLimitReached_Throws()
         {
-            account2.PaidIn = Account.PayInLimit - 100;
+            this.account2.PaidIn = Account.PayInLimit - 100;
 
-            Action action = ()=> sut.Execute(account1.Id, account2.Id, 101);
+            Action action = ()=> this.sut.Execute(this.account1.Id, this.account2.Id, 101);
 
             action.Should().Throw<InvalidOperationException>().WithMessage("Account pay in limit reached*");
         }
@@ -93,41 +91,41 @@ namespace Moneybox.App.Tests.Features
         [Fact]
         public void Execute_PayInToLimitDifferenceLessThan500_SendsNotification()
         {
-            account2.PaidIn = Account.PayInLimit - 1000;
+            this.account2.PaidIn = Account.PayInLimit - 1000;
 
-            sut.Execute(account1.Id, account2.Id, 501);
+            this.sut.Execute(this.account1.Id, this.account2.Id, 501);
 
-            notificationServiceMock.Verify(s => s.NotifyApproachingPayInLimit(account2.User.Email), Times.Once);
+            this.notificationServiceMock.Verify(s => s.NotifyApproachingPayInLimit(this.account2.User.Email), Times.Once);
         }
 
         [Fact]
         public void Execute_ValidationsPassed_UpdatesBothAccounts()
         {
-            sut.Execute(account1.Id, account2.Id, 10);
+            this.sut.Execute(this.account1.Id, this.account2.Id, 10);
 
-            updatedAccount1.Should().BeEquivalentTo(new Account
+            this.updatedAccount1.Should().BeEquivalentTo(new Account
             {
-                Id = account1.Id,
+                Id = this.account1.Id,
                 Balance = 990,
                 PaidIn = 0,
                 Withdrawn = -10,
                 User = new User
                 {
-                    Id = account1.User.Id,
+                    Id = this.account1.User.Id,
                     Email = "john.smith@test.com",
                     Name = "John Smith"
                 }
             });
 
-            updatedAccount2.Should().BeEquivalentTo(new Account
+            this.updatedAccount2.Should().BeEquivalentTo(new Account
             {
-                Id = account2.Id,
+                Id = this.account2.Id,
                 Balance = 5010,
                 PaidIn = 10,
                 Withdrawn = 0,
                 User = new User
                 {
-                    Id = account2.User.Id,
+                    Id = this.account2.User.Id,
                     Email = "mary.smith@test.com",
                     Name = "Mary Smith"
                 }
--- Moneybox.App.Tests/Features/WithdrawMoneyTests.cs
+++ Moneybox.App.Tests/Features/WithdrawMoneyTests.cs
@@ -1,7 +1,86 @@
-﻿namespace Moneybox.App.Tests.Features
+﻿using System;
+using FluentAssertions;
+using Moneybox.App.DataAccess;
+using Moneybox.App.Domain.Services;
+using Moneybox.App.Features;
+using Moq;
+using Xunit;
+
+namespace Moneybox.App.Tests.Features
 {
     public class WithdrawMoneyTests
     {
+        private WithdrawMoney sut;
+        private Mock<IAccountRepository> accountRepositoryMock;
+        private Mock<INotificationService> notificationServiceMock;
+        private Account account;
+        private Account updatedAccount;
+
+        public WithdrawMoneyTests()
+        {
+            this.account = new Account
+            {
+                Id = Guid.NewGuid(),
+                Balance = 1000,
+                PaidIn = 0,
+                Withdrawn = 0,
+                User = new User
+                {
+                    Id = Guid.NewGuid(),
+                    Email = "john.smith@test.com",
+                    Name = "John Smith"
+                }
+            };
+            this.accountRepositoryMock = new Mock<IAccountRepository>();
+            this.accountRepositoryMock
+                .Setup(r => r.GetAccountById(this.account.Id))
+                .Returns(this.account);
+
+            this.accountRepositoryMock.Setup(r => r.Update(this.account)).Callback<Account>(acc => this.updatedAccount = acc);
+
+            this.notificationServiceMock = new Mock<INotificationService>();
+
+            this.sut = new WithdrawMoney(this.accountRepositoryMock.Object, this.notificationServiceMock.Object);
+        }
+
         
+
+        [Fact]
+        public void Execute_NewBalanceIsNegative_Throws()
+        {
+            Action action = () => this.sut.Execute(this.account.Id, this.account.Balance + 1);
+
+            action.Should().Throw<InvalidOperationException>().WithMessage("Insufficient funds*");
+        }
+
+        [Fact]
+        public void Execute_NewBalanceLessThan500_SendsNotification()
+        {
+            this.account.Balance = 1000;
+
+            this.sut.Execute(this.account.Id, 501);
+
+            this.notificationServiceMock.Verify(s => s.NotifyFundsLow(this.account.User.Email), Times.Once);
+        }
+
+        [Fact]
+        public void Execute_ValidationsPassed_UpdateAccount()
+        {
+            this.sut.Execute(this.account.Id, 10);
+
+            this.updatedAccount.Should().BeEquivalentTo(new Account
+            {
+                Id = this.account.Id,
+                Balance = 990,
+                PaidIn = 0,
+                Withdrawn = -10,
+                User = new User
+                {
+                    Id = this.account.User.Id,
+                    Email = "john.smith@test.com",
+                    Name = "John Smith"
+                }
+            });
+        }
     }
 }
